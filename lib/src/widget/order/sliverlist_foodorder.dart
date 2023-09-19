5b75d9c537cbebe90702e7c6237aee374486d800
Burapa.S <74519749+burapayoky@users.noreply.github.com>
2023-09-19 17:15:01 +0700

    5.14

--- lib/src/widget/order/sliverlist_foodorder.dart
+++ lib/src/widget/order/sliverlist_foodorder.dart
@@ -21,7 +21,7 @@ class SliverListFoodOrder extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 16),
+            padding: const EdgeInsets.only(left: 24, top: 20),
             child: Align(
               alignment: Alignment.topLeft,
               child: Text(
@@ -29,7 +29,7 @@ class SliverListFoodOrder extends StatelessWidget {
                 style: const TextStyle(
                   fontSize: 38,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                  color: Colors.black54,
                 ),
               ),
             ),
@@ -62,7 +62,7 @@ class SliverListFoodOrder extends StatelessWidget {
                     children: [
                       Container(
                         height: isLandscape
-                            ? context.screenWidth / 15
+                            ? context.screenWidth / 10
                             : context.screenWidth / 7,
                         decoration: BoxDecoration(
                           color: Colors.red,
